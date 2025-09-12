f0b04d1fc64fdac1b360596baf674d497d42f42d
Ankit Kumar <ankit@192.168.1.11>
2025-09-12 15:49:22 +0530

    test

--- src/simple-twitter.py
+++ src/simple-twitter.py
@@ -1,4 +1,5 @@
 import tweepy
+import analytics
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
@@ -9,3 +10,12 @@ api = tweepy.API(auth)
 
 # Post a tweet
 api.update_status("Hello, Tweepy!")
+
+# Initialize the Analytics client
+analytics.write_key = 'YOUR_WRITE_KEY'  # Replace with your actual write key
+
+# Send an identify call
+analytics.identify(
+    user_id=pii.ad_id,       # Equivalent of pii.getAdId()
+    traits=parameters        # Dictionary of traits
+)
