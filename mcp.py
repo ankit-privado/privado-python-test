e427a280007d7abf10fe630f765314a14ec5542f
Ankit Kumar <118803988+ankit-privado@users.noreply.github.com>
2025-10-09 10:14:29 +0530

    Update mcp.py

--- mcp.py
+++ mcp.py
@@ -2,8 +2,9 @@ from anyai import MCPClient
 import json
 
 def setup_mcp_connection():
+    firstName = "name"
     mcp_server_url = "www.mcpServer.com"
     client = MCPClient("python -m mcp_server_filesystem /path/to/directory", mcp_server_url)
-    tools = client.listTools()  # <<<==== This is our expected detection
+    tools = client.listTools(firstName)  # <<<==== This is our expected detection
     print(json.dumps(tools, indent=2))
     return tools
